516908d27730742df7e6487e0fe218c9c0ffbc36
SrinithiMuruganandham <srinithi26022001@gmail.com>
2025-05-24 21:36:53 +0530

    Add files via upload
    Day1

---
+++ 02-reassignment.js
@@ -0,0 +1,17 @@
+// Using var to redeclare and reassign
+/*var nameS= 'sri';// declaration
+var nameS='nithi'// redeclaration
+//nameS='madhu'// reassign
+console.log(nameS);*/
+
+// usinglet you can't redeclare but you can reassign
+
+let nameUser= 'sri';// declaration
+//let  nameUser='nithi'// redeclaration
+nameUser='madhu' // reassign
+console.log(nameUser);
+
+// const (not able to redeclare and reassign)
+const accountNumber=1235;// declare
+//const accountNumber=98765;// redeclare
+accountNumber= 12345789 

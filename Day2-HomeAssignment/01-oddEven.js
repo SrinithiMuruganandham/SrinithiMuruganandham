c29998a262c63f14533889f0c7da813604c551e8
SrinithiMuruganandham <srinithi26022001@gmail.com>
2025-05-28 20:48:15 +0530

    Add files via upload

---
+++ Day2-HomeAssignment/01-oddEven.js
@@ -0,0 +1,16 @@
+function isOddOrEven()
+
+{
+    if(number%2===0){
+return("Even")
+    }
+    else{
+        return("odd")
+    }
+}
+
+let number=1;
+
+console.log("The number " + number+ " is " + isOddOrEven()+ ".");
+
+

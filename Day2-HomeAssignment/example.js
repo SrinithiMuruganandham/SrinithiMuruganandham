af6785c044aa61071190cfcafaef3a85e0972822
SrinithiMuruganandham <srinithi26022001@gmail.com>
2025-07-07 20:45:52 +0530

    Add files via upload

---
+++ Day2-HomeAssignment/example.js
@@ -0,0 +1,18 @@
+// let number=-9;
+
+console.log(numberType (number))
+function numberType(number){
+   number=-9;
+    
+    if(number>0){
+        return("The number is positive "+number)
+    }
+    else if(number<0){
+       return("The number is negative "+number)      
+    }
+    else{
+        return("The number is neutral "+ number)
+    }
+}
+
+

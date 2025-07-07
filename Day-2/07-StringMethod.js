cbc3b0d89a77319e61e762bcc812b8151c750d8f
SrinithiMuruganandham <srinithi26022001@gmail.com>
2025-07-07 20:41:06 +0530

    Add files via upload

---
+++ Day-2/07-StringMethod.js
@@ -0,0 +1,17 @@
+
+/* 
+
+String Declaration:
+
+1. Sting Literal
+2. Object Literal*/
+
+//String Literal - "",'',``// String pool memory same memory allocation
+
+let firstName = `Sri`;
+let userName = `Sri`;
+
+//String Object // Heap Memory it gets stored in two different memory address
+
+let firstName1 = new String("Sri");
+let userName1 = new String("Sri")
